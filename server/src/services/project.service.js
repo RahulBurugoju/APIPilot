d566fa59169054767391daf12df359e76ea859c5
RahulBurugoju <rahul9399555@gmail.com>
2026-08-25 22:17:36 +0530

    feat: implement project service layer with CRUD operations

---
+++ server/src/services/project.service.js
@@ -0,0 +1,38 @@
+import Project from "../models/project.model";
+
+const createProject = async({name,description,owner})=>{
+    const project = await Project.create({
+        name,
+        description,
+        owner
+    })
+
+    return project
+}
+
+const updateProject = async ({projectId,updatePayload}) => {
+    const project = await Project.findByIdAndUpdate(
+        projectId,
+        updatePayload,
+        { new: true, runValidators: true },
+    )
+    return project
+}
+
+
+const deleteProject = async ({projectId}) => {
+    const project = await Project.findByIdAndDelete(projectId)
+    return project
+}
+
+const getCurrentProject = async({projectId}) => {
+    const project = await Project.findById(projectId)
+    return project
+}
+
+export default {
+    createProject,
+    updateProject,
+    deleteProject,
+    getCurrentProject
+}
