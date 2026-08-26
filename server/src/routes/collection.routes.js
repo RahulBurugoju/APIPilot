0848ea1460007a8e11e25a9bca69486f8fe4fb51
RahulBurugoju <rahul9399555@gmail.com>
2026-08-26 19:18:54 +0530

    feat: implement project and collection routes with authentication and validation middleware

---
+++ server/src/routes/collection.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import collectionController from "../controllers/collection.controller.js";
+import { validate } from "../middlewares/validate.middleware.js";
+import {
+  createCollectionSchema,
+  updateCollectionSchema,
+} from "../validators/collection.validator.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
+import { verifyProjectOwner } from "../middlewares/verifyOwner.middleware.js";
+
+const router = Router({ mergeParams: true });
+
+router.use(authenticate);
+
+router
+  .route("/")
+  .all(verifyProjectOwner)
+  .post(validate(createCollectionSchema), collectionController.createCollection)
+  .get(collectionController.getProjectCollections);
+
+export default router;
--- server/src/routes/project.routes.js
+++ server/src/routes/project.routes.js
@@ -1,28 +1,30 @@
 import { Router } from "express";
 import projectController from "../controllers/project.controller.js";
-import collectionController from "../controllers/collection.controller.js";
-
+import collectionRouter from "./collection.routes.js";
 import { validate } from "../middlewares/validate.middleware.js";
-import { createProjectSchema, updateProjectSchema } from "../validators/project.validator.js";
-import { createCollectionSchema } from "../validators/collection.validator.js";
+import {
+  createProjectSchema,
+  updateProjectSchema,
+} from "../validators/project.validator.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
-import { verifyProjectOwner } from "../middlewares/verifyOwner.middleware.js";
 
 const router = Router();
 router.use(authenticate);
 
-router.route("/create").post(validate(createProjectSchema), projectController.createProject);
+// Project Collection Nested Routes
+router.use("/:projectId/collections", collectionRouter);
+
+// Project Routes
+router
+  .route("/create")
+  .post(validate(createProjectSchema), projectController.createProject);
 
 router.route("/").get(projectController.getUserProjects);
 
-router.route("/:projectId")
-.get(projectController.getProject)
-.patch(validate(updateProjectSchema),projectController.updateProject)
-.delete(projectController.deleteProject)
-
-router.route("/:projectId/collections")
-.all(verifyProjectOwner)
-.post(validate(createCollectionSchema),collectionController.createCollection)
-.get(collectionController.getProjectCollections)
+router
+  .route("/:projectId")
+  .get(projectController.getProject)
+  .patch(validate(updateProjectSchema), projectController.updateProject)
+  .delete(projectController.deleteProject);
 
 export default router;
