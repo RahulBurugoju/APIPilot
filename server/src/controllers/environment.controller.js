e7b6a2d506b4353869fa3d11505e5c2c432a98c7
RahulBurugoju <rahul9399555@gmail.com>
2026-08-30 12:59:00 +0530

    feat: implement CRUD operations and activation logic for project environments

--- server/src/controllers/environment.controller.js
+++ server/src/controllers/environment.controller.js
@@ -2,16 +2,61 @@ import environmentService from "../services/environment.service.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-// const createEnvironment = asyncHandler(async(req,res)=>{
-//     const {projectId} = 
-// })
+const createEnvironment = asyncHandler(async (req, res) => {
+    const { projectId } = req.params;
+    const { name, variables, isActive } = req.body;
+    const environment = await environmentService.createEnvironment({
+        projectId,
+        name,
+        variables,
+        isActive
+    });
+    return res.status(201)
+    .json(new ApiResponse(200, {environment},"Environment created successfully"));
+})
 
+const getProjectEnvironments = asyncHandler(async(req,res)=>{
+    const {projectId} = req.params;
+    const environments = await environmentService.getProjectEnvironments({projectId});
+    return res.status(200)
+    .json(new ApiResponse(200, {environments},"Environments fetched successfully"));
+})
 
-// export default {
-//      createEnvironment,
-//     getProjectEnvironments,
-//     getEnvironmentById,
-//     updateEnvironment,
-//     deleteEnvironment,
-//     setActiveEnvironment    
-// }
+const getEnvironmentById = asyncHandler(async(req,res)=>{
+    const {projectId, environmentId} = req.params;
+    const environment = await environmentService.getEnvironmentById({projectId,environmentId});
+    return res.status(200)
+    .json(new ApiResponse(200, {environment},"Environment fetched successfully"));
+})
+
+const updatedEnvironment = asyncHandler(async(req,res)=>{
+    const {projectId, environmentId} = req.params;
+    const { name, variables, isActive } = req.body;
+
+    const envirnoment = await environmentService.updateEnvironment({projectId,environmentId,name,variables,isActive})
+    return res.status(200)
+    .json(new ApiResponse(200,{envirnoment},"Environment updated successfully"));
+})
+
+const deleteEnvironment = asyncHandler(async(req,res)=>{
+    const {projectId, environmentId} = req.params;
+    const environment = await environmentService.deleteEnvironment({projectId,environmentId});
+    return res.status(200)
+    .json(new ApiResponse(200, {environment},"Environment deleted successfully"));
+})
+
+const setActiveEnvironment = asyncHandler(async(req,res)=>{
+    const {projectId, environmentId} = req.params;
+    const environment = await environmentService.setActiveEnvironment({projectId,environmentId});
+    return res.status(200)
+    .json(new ApiResponse(200, {environment},"Environment activated successfully"));
+})
+
+export default {
+    createEnvironment,
+    getProjectEnvironments,
+    getEnvironmentById,
+    updateEnvironment,
+    deleteEnvironment,
+    setActiveEnvironment
+}
--- server/src/routes/environment.routes.js
+++ server/src/routes/environment.routes.js
@@ -1,26 +1,26 @@
 import { Router } from "express";
-
+import environmentController from "../controllers/environment.controller.js";
 const router = Router({ mergeParams: true });
 
 // POST   /projects/:projectId/environments
 // GET    /projects/:projectId/environments
 router
   .route("/")
-  .post((req, res) => {})
-  .get((req, res) => {});
+  .post(environmentController.createEnvironment)
+  .get(environmentController.getProjectEnvironments);
 
 // GET    /projects/:projectId/environments/:environmentId
 // PATCH  /projects/:projectId/environments/:environmentId
 // DELETE /projects/:projectId/environments/:environmentId
 router
   .route("/:environmentId")
-  .get((req, res) => {})
-  .patch((req, res) => {})
-  .delete((req, res) => {});
+  .get(environmentController.getEnvironmentById)
+  .patch(environmentController.updateEnvironment)
+  .delete(environmentController.deleteEnvironment);
 
 // PATCH  /projects/:projectId/environments/:environmentId/activate
 router
   .route("/:environmentId/activate")
-  .patch((req, res) => {});
+  .patch(environmentController.setActiveEnvironment);
 
 export default router;
